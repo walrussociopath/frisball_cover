21538783045a81fa751d5ed96f578821257d0c72
Nikolay Ignatiev <walrussociopath@gmail.com>
2026-06-06 18:56:09 +0300

    globe pngs

--- earth/main.py
+++ earth/main.py
@@ -1,25 +1,32 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import numpy as np
 
-# Создаем проекцию "вид на шар" (ортографическая)
-fig, ax = plt.subplots(figsize=(10, 10),
-                       subplot_kw={'projection': ccrs.NearsidePerspective(
-                           central_longitude=18, central_latitude=40,
-                           satellite_height=2000000
-                       )})
 
-# --- Магия здесь: добавляем готовые географические объекты ---
-# Заливаем океан (он будет фоном)
-ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
-# Добавляем контуры материков
-ax.add_feature(cfeature.COASTLINE, linewidth=0.8)
-# Заливаем сушу (опционально, для наглядности)
-ax.add_feature(cfeature.LAND, facecolor='lightgray', alpha=0.7)
+def draw(long, lat):
+    fig, ax = plt.subplots(figsize=(10, 10),
+                        subplot_kw={'projection': ccrs.NearsidePerspective(
+                            central_longitude=long, central_latitude=lat,
+                            satellite_height=2000000
+                        )})
 
-# Показываем весь глобус
-ax.set_global()
-ax.gridlines()
+    # Добавляем контуры материков
+    ax.add_feature(cfeature.COASTLINE, linewidth=0.8, color='black')
 
-plt.title("Контуры материков на глобусе")
-plt.show()
+    # Заливаем сушу
+    ax.add_feature(cfeature.LAND, facecolor='lightgray', alpha=0.7)
+
+    # Показываем весь глобус
+    ax.set_global()
+
+    # Сохраняем с прозрачным фоном и без сетки
+    plt.savefig(f'globe_nudes/{long}_{lat}.png', 
+                dpi=300, 
+                transparent=True,      # Файл PNG с прозрачностью
+                bbox_inches='tight',
+                facecolor='none')
+
+
+if __name__ == '__main__':
+    draw(22, 38)
